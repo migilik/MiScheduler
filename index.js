ae120521cf1ae3e12fb112998b9ca2a5ba7f7521
Michael Gilik <migilik@gmail.com>
2017-03-21 22:13:20 +0900

    Basic sql insert/get test, see / and /test endpoints.

--- index.js
+++ index.js
@@ -1,11 +1,53 @@
 var express = require("express");
+var sqlite3 = require("sqlite3");
+var fs = require("fs");
+
 var app = express();
 var port = 3000;
 
+var promisesBeforeServerStart = [];
+
+function readFileAsPromise(filePath) {
+  return new Promise(function (resolve, reject) {
+    fs.readFile(filePath, "utf8", function (err, fileData) {
+      if (err) { reject(err); }
+      else { resolve(fileData); }
+    });
+  });
+};
+
+var sqlFiles = ["getUsers", "insertUser"];
+var sql = {};
+var sqlReadPromises = sqlFiles
+  .map(s => "./sql/" + s + ".sql")
+  .map(readFileAsPromise)
+  .map((p, i) => p.then(fileData => sql[sqlFiles[i]] = fileData));
+
+promisesBeforeServerStart = promisesBeforeServerStart.concat(sqlReadPromises);
+
+var db = new sqlite3.Database("db.sqlite");
+
+// basic test, visit /test to insert dummy user and / to dump all the users
 app.get("/", function (req, res) {
-  res.send("Hello world!");
+  db.serialize(function() {
+    var rows = [];
+    db.all(sql.getUsers, function(err, rows) {
+      res.send(rows.map(r => r.full_name).join(" - "));
+    });
+  });
 });
 
-app.listen(port, function () {
-  console.log("Listening on port ".concat(port));
+app.get("/test", function (req, res) {
+  db.serialize(function() {
+    var statement = db.prepare(sql.insertUser);
+    statement.run("User McTest");
+    statement.finalize();
+  });
+  res.send("Test insert.");
+});
+
+Promise.all(promisesBeforeServerStart).then(() => {
+  app.listen(port, function () {
+    console.log("Listening on port ".concat(port));
+  });
 });
---
+++ sql/insertUser.sql
@@ -0,0 +1,1 @@
+INSERT INTO users (full_name) VALUES (?);
